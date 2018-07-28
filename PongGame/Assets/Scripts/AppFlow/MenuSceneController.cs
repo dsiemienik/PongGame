66bbb7427cda8a306fcc201ee67da02f9f908411
DSiemienik <dsiemienik@gmail.com>
2018-07-28 18:00:35 +0200

    Menu scene controller

---
+++ PongGame/Assets/Scripts/AppFlow/MenuSceneController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSceneController : MonoBehaviour {
+
+    private void Start()
+    {
+        PlayerPrefs.SetInt("difficulty", -1);
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        SceneLoadingController.Instance.LoadScene((GameScenes)sceneIndex, true);
+    }
+    public void LoadSceneWithCPU(int difficulty)
+    {
+        PlayerPrefs.SetInt("difficulty", difficulty);
+        LoadScene(1);
+    }
+}
