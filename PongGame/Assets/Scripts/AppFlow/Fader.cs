c738d58e41180a2a1a26a20a37973418b2e945cf
DSiemienik <dsiemienik@gmail.com>
2018-07-28 18:00:49 +0200

    UI fader

---
+++ PongGame/Assets/Scripts/AppFlow/Fader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class Fader : MonoBehaviour {
+
+    public float fadingSpeed;
+
+    CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+
+    }
+    public void ShowUI()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeOutUI(alphaTarget: 1f));
+    }
+    public void HideUI()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeInUI(alphaTarget: 0f));
+    }
+    IEnumerator FadeInUI(float alphaTarget)
+    {
+        while(canvasGroup.alpha >= alphaTarget)
+        {
+            canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, alphaTarget, fadingSpeed);
+            yield return null;
+        }
+    }
+    IEnumerator FadeOutUI(float alphaTarget)
+    {
+        while (canvasGroup.alpha <= alphaTarget)
+        {
+            canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, alphaTarget, fadingSpeed);
+            yield return null;
+        }
+    }
+}
